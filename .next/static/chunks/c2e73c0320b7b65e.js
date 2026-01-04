fc601ebc3a805131fa20adff6536758fbd9735f4
Bear Miner Dev <dev@bearminer.com>
2026-01-04 17:36:55 +0100

    deploy: v7 style recovery and hosting optimization build

--- .next/static/chunks/c2e73c0320b7b65e.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/ab4ab76244c00380.js",
-  "static/chunks/turbopack-28eba19c21ac8121.js"
-])
---
+++ .next/static/chunks/d6226f700aa2871b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/bd7611464aaba5ba.js",
+  "static/chunks/turbopack-5501d53f840dc0d0.js"
+])
